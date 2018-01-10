8ef1cba57419d2600f3e9ff8d07c1144e154c8fc
Jeff Shindelus <jeff.shindelus@gmail.com>
2018-01-10 09:52:48 -0800

    add runtime.txt

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def upload_file():
     '''
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 33507))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
